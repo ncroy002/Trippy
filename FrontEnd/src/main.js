d429b8f57d2d2f3be4193531e24e097095a3da35
Natalie <nataliecroy92@gmail.com>
2020-09-18 15:58:07 -0500

    created FAQ page

---
+++ FrontEnd/src/main.js
@@ -0,0 +1,33 @@
+// =========================================================
+// * Vue Material Kit - v1.2.2
+// * Copyright 2019 Creative Tim (https://www.creative-tim.com)
+// * Licensed under MIT (https://github.com/creativetimofficial/vue-material-kit/blob/master/LICENSE.md)
+// =========================================================
+
+
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+import MaterialKit from "./plugins/material-kit";
+
+Vue.config.productionTip = false;
+
+Vue.use(MaterialKit);
+
+const NavbarStore = {
+  showNavbar: false
+};
+
+Vue.mixin({
+  data() {
+    return {
+      NavbarStore
+    };
+  }
+});
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
