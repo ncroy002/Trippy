17cefd62fd2b1ea9ce1c0daf8bc0662c12ff1946
Utsav Robby Walters <uwalters@uco.edu>
2020-09-23 23:49:28 -0500

    Added validation for the login.vue
    If the password is not 6 characters long and the if the emails is not in the right format the user is denied entry. However, if the user enter the right credentials and the request for login is sent to the back end server.

--- Front End/src/views/Login.vue
+++ Front End/src/views/Login.vue
@@ -12,15 +12,45 @@
               <md-field class="md-form-group" slot="inputs">
                 <md-icon>email</md-icon>
                 <label>Email...</label>
-                <md-input v-model="email" type="email"></md-input>
+                <md-input
+                  aria-required="true"
+                  type="email"
+                  v-model="email"
+                ></md-input>
               </md-field>
               <md-field class="md-form-group" slot="inputs">
                 <md-icon>lock_outline</md-icon>
                 <label>Password...</label>
-                <md-input v-model="password"></md-input>
+                <md-input
+                  aria-required="true"
+                  type="password"
+                  v-model="password"
+                ></md-input>
               </md-field>
-              <md-button v-on:click="login()" slot="footer" class="md-simple md-success md-lg">Login</md-button>
+              <md-button
+                v-on:click="login()"
+                slot="footer"
+                class="md-simple md-success md-lg"
+                >Login</md-button
+              >
             </login-card>
+
+            <div v-if="valid" class="alert alert-danger">
+              <div class="container">
+                <button
+                  type="button"
+                  aria-hidden="true"
+                  class="close"
+                  @click="event => removeNotify(event, 'alert-danger')"
+                >
+                  <md-icon>clear</md-icon>
+                </button>
+                <div class="alert-icon">
+                  <md-icon>info_outline</md-icon>
+                </div>
+                <b> ERROR ALERT </b> : Please enter a valid username and password.
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -41,7 +71,8 @@ export default {
   data() {
     return {
       email: null,
-      password: null
+      password: null,
+      valid: false
     };
   },
   props: {
@@ -52,20 +83,43 @@ export default {
   },
   methods: {
     login() {
-      const url = "http://localhost:8080/user/login";
-      const account = new Account(this.email, this.password);
-      Axios.post(url, account, {params: {
-        header: {
-          "Content-Type": "application/json",
+      const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (this.email && this.password) {
+        if (
+          this.password.length >= 6 &&
+          re.test(String(this.email).toLowerCase())
+        ) {
+          this.valid = false;
+          const url = "http://localhost:8080/user/login";
+          const account = new Account(this.email, this.password);
+          Axios.post(url, account, {
+            params: {
+              header: {
+                "Content-Type": "application/json"
+              }
+            }
+          })
+            .then(reponse => {
+              console.log(reponse);
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        } else {
+          this.valid = true;
         }
-      }})
-        .then(reponse => {
-          console.log(reponse);
-        })
-        .catch(error => {
-          console.log(error);
-        });
+      } else {
+        this.valid = true;
+      }
+    },
+    removeNotify(e, notifyClass){
+      var target = e.target;
+      while(target.className.indexOf(notifyClass) === -1){
+        target = target.parentNode;
+      }
+      return target.parentNode.removeChild(target);
     }
+
   },
   computed: {
     headerStyle() {
