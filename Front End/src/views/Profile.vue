2caac0f452ba230a1ba144f3181150a7fe013a29
papa s. diaw <diaw.psd@gmail.com>
2020-09-27 16:13:03 -0500

    added profile changes

--- Front End/src/views/Profile.vue
+++ Front End/src/views/Profile.vue
@@ -38,6 +38,55 @@
               <p>{{user.descrip}}</p>
             </div>
           </div>
+                    <div class="profile-tabs">
+            <tabs
+              :tab-name="['Recently viewed', 'Shared trips', 'Favorite']"
+              :tab-icon="['explore', 'share', 'favorite']"
+              plain
+              nav-pills-icons
+              color-button="success"
+            >
+              <!-- here you can add your content for tab-content -->
+              <template slot="tab-pane-1">
+                <div class="md-layout">
+                  <div class="md-layout-item md-size-25 ml-auto">
+                    <img :src="tabPane1[0].image" class="rounded" />
+                    <img :src="tabPane1[1].image" class="rounded" />
+                  </div>
+                  <div class="md-layout-item md-size-25 mr-auto">
+                    <img :src="tabPane1[3].image" class="rounded" />
+                    <img :src="tabPane1[2].image" class="rounded" />
+                  </div>
+                </div>
+              </template>
+              <template slot="tab-pane-2">
+                <div class="md-layout">
+                  <div class="md-layout-item md-size-25 ml-auto">
+                    <img :src="tabPane2[0].image" class="rounded" />
+                    <img :src="tabPane2[1].image" class="rounded" />
+                    <img :src="tabPane2[2].image" class="rounded" />
+                  </div>
+                  <div class="md-layout-item md-size-25 mr-auto">
+                    <img :src="tabPane2[3].image" class="rounded" />
+                    <img :src="tabPane2[4].image" class="rounded" />
+                  </div>
+                </div>
+              </template>
+              <template slot="tab-pane-3">
+                <div class="md-layout">
+                  <div class="md-layout-item md-size-25 ml-auto">
+                    <img :src="tabPane3[0].image" class="rounded" />
+                    <img :src="tabPane3[1].image" class="rounded" />
+                  </div>
+                  <div class="md-layout-item md-size-25 mr-auto">
+                    <img :src="tabPane3[2].image" class="rounded" />
+                    <img :src="tabPane3[3].image" class="rounded" />
+                    <img :src="tabPane3[4].image" class="rounded" />
+                  </div>
+                </div>
+              </template>
+            </tabs>
+          </div>
           <md-button @click='toggle = !toggle' v-on:click="updateUserDetails" class="md-warning">{{toggle ? 'save' : 'edit'}}</md-button>
         </div>
       </div>
@@ -47,11 +96,12 @@
 
 <script>
 import axios from 'axios'
+import { Tabs } from "@/components";
 export default {
   components: {
+    Tabs
   },
   bodyClass: "profile-page",
-
   data() {
     return {
       toggle: false,
@@ -62,7 +112,27 @@ export default {
         email:'',
         id:''
       },
-    }
+      tabPane1: [
+        { image: require("@/assets/img/examples/studio-1.jpg") },
+        { image: require("@/assets/img/examples/studio-2.jpg") },
+        { image: require("@/assets/img/examples/studio-4.jpg") },
+        { image: require("@/assets/img/examples/studio-5.jpg") }
+      ],
+      tabPane2: [
+        { image: require("@/assets/img/examples/olu-eletu.jpg") },
+        { image: require("@/assets/img/examples/clem-onojeghuo.jpg") },
+        { image: require("@/assets/img/examples/cynthia-del-rio.jpg") },
+        { image: require("@/assets/img/examples/mariya-georgieva.jpg") },
+        { image: require("@/assets/img/examples/clem-onojegaw.jpg") }
+      ],
+      tabPane3: [
+        { image: require("@/assets/img/examples/mariya-georgieva.jpg") },
+        { image: require("@/assets/img/examples/studio-3.jpg") },
+        { image: require("@/assets/img/examples/clem-onojeghuo.jpg") },
+        { image: require("@/assets/img/examples/olu-eletu.jpg") },
+        { image: require("@/assets/img/examples/studio-1.jpg") }
+      ]
+    };
   },
   props: {
     header: {
@@ -118,16 +188,13 @@ export default {
 .section {
   padding: 0;
 }
-
 .profile-tabs::v-deep {
   .md-card-tabs .md-list {
     justify-content: center;
   }
-
   [class*="tab-pane-"] {
     margin-top: 3.213rem;
     padding-bottom: 50px;
-
     img {
       margin-bottom: 2.142rem;
     }
