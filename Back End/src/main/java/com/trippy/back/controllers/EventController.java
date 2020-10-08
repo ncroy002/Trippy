97e8cf34584269ea6527b88d92ca1534559933d3
murrehman1 <muhammad_saif1998@outlook.com>
2020-10-08 00:47:21 -0500

    events-backend

--- Back End/src/main/java/com/trippy/back/controllers/EventController.java
+++ Back End/src/main/java/com/trippy/back/controllers/EventController.java
@@ -1,4 +1,63 @@
 package com.trippy.back.controllers;
 
+
+import com.trippy.back.com.trippy.back.util.ImageUtil;
+import com.trippy.back.entities.Event;
+import com.trippy.back.repos.EventsRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping(value = "/event")
+
+
 public class EventController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    EventsRepo eventRepository;
+
+    @PostMapping(value = "/create", consumes = "application/json;charset=UTF-8" )
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity create(@RequestBody Event event){
+        eventRepository.save(event);
+        return new ResponseEntity(event, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/edit")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity edit(@RequestBody Event event){
+        Optional<Event> eventFound = eventRepository.findById(event.getId());
+        if(eventFound.isPresent()){
+            eventRepository.save(event);
+            return new ResponseEntity(HttpStatus.OK);
+        }
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping(value = "/delete/{eventId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity delete(@PathVariable("eventId") int id){
+        Optional<Event> event = eventRepository.findById(id);
+
+        if(event.isPresent()){
+            eventRepository.delete(event.get());
+            return new ResponseEntity(HttpStatus.OK);
+        }
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping(value = "/list")
+    public ResponseEntity list(){
+        return new ResponseEntity<>(eventRepository.findAll(), HttpStatus.OK);
+    }
 }
--- Back End/src/main/java/com/trippy/back/repos/EventsRepo.java
+++ Back End/src/main/java/com/trippy/back/repos/EventsRepo.java
@@ -1,4 +1,15 @@
 package com.trippy.back.repos;
 
-public class EventsRepo {
+import com.trippy.back.entities.Account;
+import com.trippy.back.entities.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EventsRepo extends JpaRepository<Event, Integer> {
+//    Optional<Event> findByIDOptional(int ID);
+//    Boolean existsByID(int ID);
+//    Event findByID(int ID);
 }
