65e25191cc9d37a7fc2e46ab071fc465d0fa7f0a
Andrew King <aking@inspirebrands.com>
2020-10-22 20:10:04 -0500

    backend files

--- Back End/src/main/java/com/trippy/back/services/FilterService.java
+++ Back End/src/main/java/com/trippy/back/services/FilterService.java
@@ -1,2 +1,38 @@
-package com.trippy.back.services;public class FilterService {
+package com.trippy.back.services;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import com.squareup.okhttp.ResponseBody;
+
+import com.trippy.back.BackApplication;
+import net.minidev.json.JSONObject;
+
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class FilterService {
+    OkHttpClient client = new OkHttpClient();
+
+    public String generateInterestResults(String destination, String interest) throws IOException {
+
+        Request request = new Request.Builder()
+                .url("https://api.geoapify.com/v2/places?categories=" + interest +
+                        "&filter=circle:-81.3790304,28.5421109,50000&bias=proximity:-81.3790304,28.5421109&limit=12&apiKey=e74299088e09495bb2120458d0ac22e2")
+                //.url("https://api.geoapify.com/v2/places?categories=" + interest +
+                //        "&filter=geometry:a93efddbfe77d592b8c42b376494bf24&bias=proximity:-81.3790304,28.5421109&lang=en&limit=12&apiKey=e74299088e09495bb2120458d0ac22e2")
+                .get()
+                .build();
+
+        Response response = client.newCall(request).execute();
+        String jsonData = response.body().string();
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(jsonData);
+        return(jsonData);
+    }
+
 }
